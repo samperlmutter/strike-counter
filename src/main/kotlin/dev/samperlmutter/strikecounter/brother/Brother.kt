65574f237c1574e2b7f07581c6e1032e93748251
Sam Perlmutter <sam.perlmutter227@gmail.com>
2019-11-24 23:10:09 -0500

    more work on functions added a can_strike coulumn so only authorized brothers can strike

--- src/main/kotlin/dev/samperlmutter/strikecounter/brother/Brother.kt
+++ src/main/kotlin/dev/samperlmutter/strikecounter/brother/Brother.kt
@@ -8,9 +8,13 @@ import javax.validation.constraints.NotNull
 
 @Entity
 @Table(name = "brothers")
-class Brother (@Id @Column(name = "id", nullable = false, length = 200) @NotNull
-               var slackId: String,
-               @Column(name = "full_name", nullable = false, length = 200) @NotNull
-               var fullName: String,
-               @Column(name = "strikes", nullable = false)
-               var strikes: Int)
+class Brother(
+    @Id @Column(name = "id", nullable = false, length = 200) @NotNull
+    var slackId: String,
+    @Column(name = "full_name", nullable = false, length = 200) @NotNull
+    var fullName: String,
+    @Column(name = "can_strike", nullable = false, length = 1) @NotNull
+    var canStrike: Int,
+    @Column(name = "strikes", nullable = false)
+    var strikes: Int
+)
--- src/main/kotlin/dev/samperlmutter/strikecounter/brother/BrotherController.kt
+++ src/main/kotlin/dev/samperlmutter/strikecounter/brother/BrotherController.kt
@@ -13,4 +13,47 @@ class BrotherController() {
     constructor(brotherRepository: BrotherRepository) : this() {
         this.brotherRepository = brotherRepository
     }
+
+    @RequestMapping(method = [RequestMethod.POST], name = "/strokes")
+    fun cmdHandler(keyVals: HashMap<String, Any>) {
+        val brother = brotherRepository.findBySlackId(keyVals["user_id"] as String)
+        val params = (keyVals["text"] as String).split(' ')
+
+        when (keyVals["command"]) {
+            "/stroke" -> strike(brother, params)
+            "/unstroke" -> unstrike(brother, params)
+            "/strokes" -> strikes()
+        }
+    }
+
+    private fun strike(caller: Brother, params: List<String>) {
+        if (caller.canStrike == 1) {
+            for (param in params) {
+                var brother = brotherRepository.findBySlackId(param)
+                brother.strikes++
+                brotherRepository.save(brother)
+            }
+            // TODO: return a happy message
+        } else {
+            // TODO: inform user they don't have access
+        }
+    }
+
+    private fun unstrike(caller: Brother, params: List<String>) {
+        if (caller.canStrike == 1) {
+            for (param in params) {
+                var brother = brotherRepository.findBySlackId(param)
+                brother.strikes--
+                brotherRepository.save(brother)
+            }
+            // TODO: return a happy message
+        } else {
+            // TODO: inform user they don't have access
+        }
+    }
+
+    private fun strikes() {
+        val brothers = brotherRepository.findAll()
+        // TODO: return info
+    }
 }
